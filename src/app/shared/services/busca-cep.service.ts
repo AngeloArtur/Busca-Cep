74ef3c62ff183f93e3d92a1594fab5c9f4e26fee
Ângelo Artur <ang.vianna@gmail.com>
2024-07-14 22:00:51 -0300

    style: rename method

--- src/app/shared/services/busca-cep.service.ts
+++ src/app/shared/services/busca-cep.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICep } from '../../core/interfaces/icep';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,7 @@ export class BuscaCepService {
 
   constructor(private http: HttpClient) {}
 
-  public returnCep(cep: string) {
+  public returnCep(cep: string):Observable<ICep> {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
     return this.http.get(url);
   }
