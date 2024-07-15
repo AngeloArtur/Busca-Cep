46c3e2380af1a28de834ca7d8affb7edae243fd7
Ângelo Artur <ang.vianna@gmail.com>
2024-07-15 20:53:07 -0300

    feat: using cepData and SweetAlert

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -14,6 +14,8 @@ import { CepDirective } from '../../shared/directives/cep.directive';
 import { Router, RouterLink } from '@angular/router';
 import { ICep } from '../../core/interfaces/icep';
 import { NgClass } from '@angular/common';
+import { CepDataService } from '../../shared/services/cep-data.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -37,19 +39,29 @@ export class HomeComponent {
   constructor(
     private cepService: BuscaCepService,
     private router: Router,
+    private cepData: CepDataService
   ) {}
 
   @ViewChild('cepInput') cep!: ElementRef;
 
   public searchCep(cep: string) {
     const cepNumbers = cep.replace('-', '');
-    if (cep.length === 0) {
+    if (cep.length === 0 || cep.length < 8) {
       this.isInvalid = true;
-      console.log(this.isInvalid)
     } else {
       this.cepService.returnCep(cepNumbers).subscribe((value: ICep) => {
-        console.log(value);
-        this.router.navigate(['/cep']), { state: { cepData: value } };
+        if(!('erro' in value)) {
+          this.cepData.setCepData(value);
+          this.router.navigate(['/cep']);
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Cep não encontrado',
+            confirmButtonColor: '#5ABFAD',
+          });
+          this.cep.nativeElement.value = "";
+        }
       });
     }
   }
