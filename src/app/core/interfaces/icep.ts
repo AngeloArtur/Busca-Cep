10ae1c231ef3c037697dd19deb2b14f2c56c7692
Ângelo Artur <ang.vianna@gmail.com>
2024-07-14 21:59:29 -0300

    new: interface for cep data

---
+++ src/app/core/interfaces/icep.ts
@@ -0,0 +1,13 @@
+export interface ICep {
+  cep?: string;
+  logradouro?: string;
+  bairro?: string;
+  cidade?: string;
+  estado?: string;
+  complemento?: string;
+  ddd?: string;
+  gia?: string;
+  ibge?: string;
+  siafi?: string;
+  uf?: string;
+}
